455f775bdc986315b0c8c014e5d5e83757ae7f7c
Sumit Kushwah <sumitkushwah1729@gmail.com>
2024-03-10 21:55:07 +0530

    add remote check

--- check_git_status.py
+++ check_git_status.py
@@ -22,15 +22,31 @@ def check_commit_needed(path: str) -> bool:
     return True
 
 
+def check_if_remote_exists(path: str) -> bool:
+    os.chdir(path)
+    result = os.popen("git remote -v").read()
+    if "origin" in result:
+        return True
+    return False
+
+
 uncommitted_dirs = []
+unremote_dirs = []
 
 for root, dirs, files in os.walk(directory):
     if ".git" in dirs:
         print(f"Checking {root}")
         if check_commit_needed(root):
             uncommitted_dirs.append(root)
+        if not check_if_remote_exists(root):
+            unremote_dirs.append(root)
 
 if uncommitted_dirs:
-    print("The following directories have uncommitted changes:\n")
+    print("\nThe following directories have uncommitted changes:")
     for d in uncommitted_dirs:
+        print(d)
+
+if unremote_dirs:
+    print("\nThe following directories do not have a remote:")
+    for d in unremote_dirs:
         print(d)
