25713f4b0f6a7c6ce0d139dcb3a69fa7b75a62d8
Sumit Kushwah <sumitkushwah1729@gmail.com>
2024-03-14 14:23:41 +0530

    Auto commit at 2024-03-14 14:23:41.814606

--- install.py
+++ install.py
@@ -7,23 +7,32 @@ with open("commands.json", "r") as myfile:
 
 # parse file
 commands = json.loads(data)
+cwd = os.getcwd()
 
+# clean all the aliases from .bashrc and .zshrc
 for command in commands:
-    cwd = os.getcwd()
-    full_path = os.path.join(cwd, command["file"])
-    alias_command = f"alias {command['alias']}=\"{command['program']} {full_path}\""
-    # check .bashrc exist or not
-    if os.path.isfile(os.path.expanduser("~/.bashrc")):
-        with open(os.path.expanduser("~/.bashrc"), "a") as myfile:
-            myfile.write(f"\n{alias_command}")
-            print(f"Added {alias_command} to .bashrc")
-    else:
-        print("No .bashrc file found")
+    reg_exp = f"^alias.*{command['alias']}=.*"
+    os.system(f"sed -i '/{reg_exp}/d' ~/.bashrc")
+    os.system(f"sed -i '/{reg_exp}/d' ~/.zshrc")
 
-    # check .zshrc exist or not
-    if os.path.isfile(os.path.expanduser("~/.zshrc")):
-        with open(os.path.expanduser("~/.zshrc"), "a") as myfile:
-            myfile.write(f"\n{alias_command}")
-            print(f"Added {alias_command} to .zshrc")
+# writing to .zshrc
+# check .zshrc exist or not
+for file in [os.path.expanduser("~/.zshrc"), os.path.expanduser("~/.bashrc")]:
+    filename = os.path.basename(file)
+    if os.path.isfile(file):
+        cwd_with_backslashes = cwd.replace("/", "\/")
+        sedcommand = f"sed -i '/# Added by {cwd_with_backslashes}\/install.py/d' {file}"
+        os.system(sedcommand)
+        with open(file, "a") as myfile:
+            myfile.write(f"# Added by {cwd}/install.py\n")
+            alias_commands = []
+            for command in commands:
+                full_path = os.path.join(cwd, command["file"])
+                alias_command = (
+                    f"alias {command['alias']}=\"{command['program']} {full_path}\""
+                )
+                alias_commands.append(alias_command)
+                print(f"Added {alias_command} to {filename}")
+            myfile.write("\n".join(alias_commands))
     else:
-        print("No .zshrc file found")
+        print(f"No {filename} file found")
