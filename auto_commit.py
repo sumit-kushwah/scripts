e17be78b4a19401fad771c72667c8eaec590440a
Sumit Kushwah <sumitkushwah1729@gmail.com>
2024-03-24 21:35:36 +0530

    add flag for all repos

--- auto_commit.py
+++ auto_commit.py
@@ -1,7 +1,7 @@
 import os
 import datetime
 import argparse
-from helpers import get_uncommitted_dirs
+from helpers import get_uncommitted_dirs, get_all_git_dirs
 
 parser = argparse.ArgumentParser(
     description="Auto commit all git repositories which have uncommitted changes"
@@ -24,6 +24,14 @@ parser.add_argument(
     default="/home/sumit/Development/",
 )
 
+parser.add_argument(
+    "--force",
+    "-f",
+    type=bool,
+    help="force push to origin",
+    default=False,
+)
+
 # get branch
 parser.add_argument(
     "--branch",
@@ -37,14 +45,18 @@ args = parser.parse_args()
 commit_message = args.message
 directory = args.dir
 branch = args.branch
+force = args.force
+
+if force:
+    print("Force push enabled")
 
 uncommitted_dirs = get_uncommitted_dirs(directory)
-
 if len(uncommitted_dirs) == 0:
     print("No uncommitted changes found")
-    exit(0)
-
-print(f"Committing uncommited changes with message: {commit_message}")
+    if not force:
+        exit(0)
+else:
+    print(f"Committing uncommited changes with message: {commit_message}")
 
 for d in uncommitted_dirs:
     os.chdir(d)
@@ -52,3 +64,12 @@ for d in uncommitted_dirs:
     os.system(f'git commit -m "{commit_message}"')
     os.system("git push origin " + branch)
     print(f"Pushed {d} to origin/{branch}")
+
+print("Force push other git repositories")
+
+gitrepos = get_all_git_dirs(directory)
+
+for d in gitrepos:
+    os.chdir(d)
+    os.system(f"git push origin {branch}")
+    print(f"Force pushed {d} to origin/{branch}")
